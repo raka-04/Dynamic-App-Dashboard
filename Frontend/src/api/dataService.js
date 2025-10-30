ebd08b0fb963bf2ad417529aafb8cf05dba22a52
raka-04 <soukarya2022@gmail.com>
2025-10-30 21:11:28 +0530

    update in code

--- Frontend/src/api/dataService.js
+++ Frontend/src/api/dataService.js
@@ -19,7 +19,6 @@ export const fetchTableData = async () => {
     const response = await axios.get(JSONPLACEHOLDER_URL);
     return response.data;
   } catch (error) {
-    console.error('Error fetching table data:', error);
-    throw new Error('Could not load data grid records.');
+    
   }
 };
